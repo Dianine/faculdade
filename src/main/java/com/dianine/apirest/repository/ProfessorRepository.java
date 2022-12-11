fb4c71a8e6fe50465bfeb4188c2722eadc53f6e9
Dianine <dianine23@gmail.com>
2022-12-11 11:43:59 -0300

    criacao do reposity professor

---
+++ src/main/java/com/dianine/apirest/repository/ProfessorRepository.java
@@ -0,0 +1,9 @@
+package com.dianine.apirest.repository;
+
+import com.dianine.apirest.model.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Integer> {
+}
